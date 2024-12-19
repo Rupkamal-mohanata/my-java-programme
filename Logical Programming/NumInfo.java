da0445b61102a4c80996e8d226a3ba791c041b25
Rupkamal <mohantarupkamal604@gmail.com>
2024-12-20 00:34:16 +0530

    NumInfo.java created

---
+++ Logical Programming/NumInfo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class NumInfo{
+	//main method
+    public static void main(String[] args) {
+        
+		// variable declaration
+		int num ;
+		
+		// created Scanner Object "scanner"
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter a number ");
+		num = scanner.nextInt();
+		
+        if(num == 0){
+            System.out.println("Number is Zero");
+        }else if(num >0){
+            System.out.println(num+" is Postive ");
+        }else{
+            System.out.println(num+"is Negative");
+        }
+    }
+}
